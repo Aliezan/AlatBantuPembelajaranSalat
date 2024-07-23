17c3aa5b671d0191c8873a2e66e4705096accebd
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-07-23 17:31:27 +0700

    Completed application state

---
+++ Assets/C# Scripts/PendahuluanNav.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PendahuluanNav : MonoBehaviour
+{
+    public Button NextNavBtn;
+    public Button BackToMenuBtn;
+
+    void Start()
+    {
+        NextNavBtn.onClick.AddListener(() => GoToMateriShalat("Shubuh"));
+        BackToMenuBtn.onClick.AddListener(() => GoToMateriShalat("MainMenu"));
+    }
+
+    void GoToMateriShalat(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
